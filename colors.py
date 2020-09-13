300f76bee6fa603c3b982c9097cd8804791e2406
Jacob Sabella <jacobsabella@yahoo.com>
2020-09-12 21:18:39 -0500

    added adafruit-blinka

--- colors.py
+++ colors.py
@@ -5,7 +5,7 @@ PURPLE = (0, 75, 130)
 ORANGE = (106, 253, 2)
 WHITE = (255, 255, 255)
 MAROON = (0, 128, 0)
-YELLOW = (255, 255, 0)
+YELLOW = (175, 255, 0)
 OFF = (0, 0, 0)
 
 COLORS = {"red": RED, "green": GREEN, "blue": BLUE, "purple": PURPLE, "orange": ORANGE, "white": WHITE,
