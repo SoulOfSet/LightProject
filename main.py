c00be2fffdf1519abd9bc67f0d502d33fb95e5ea
Jacob Sabella <jacobsabella@yahoo.com>
2020-09-12 20:37:07 -0500

    Added requirements and config file

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import os
+
 import board
 import flask
+import yaml
 from flask import Flask, request, jsonify
+from py_eureka_client import eureka_client
 
 import colors
 from gpio.led_manager import LedManager
@@ -14,6 +18,24 @@ except ImportError:
 
 # Create Flask object called app.
 app = Flask(__name__)
+
+# Directory for config file
+config_dir = os.path.dirname(os.path.realpath(__file__)) + os.sep
+
+# Attempt to load the config file and assign it to the appropriate variables
+hostname = port = app_name = eureka_port = eureka_hostname = None
+
+try:
+    with open(config_dir + os.sep + "config.yml") as config_file:
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
+        hostname = config['hostname']
+        port = config['port']
+        app_name = config['app_name']
+        eureka_port = config['eureka_port']
+        eureka_hostname = config['eureka_hostname']
+except Exception as e:
+    print("Unable to load config file")
+    exit(1)
 
 curr_thread = None
 
@@ -54,4 +76,7 @@ def get_colors():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    eureka_client.init_registry_client(eureka_server="http://" + eureka_hostname + ":" + eureka_port + "/eureka",
+                                       app_name=app_name,
+                                       instance_port=port)
+    app.run(host=hostname, port=port)
