50ce793b6cb1bd964e55b374503e366d8a80d7e1
Jacob Sabella <jacobsabella@yahoo.com>
2020-10-04 11:25:03 -0500

    Changed colors

--- colors.py
+++ colors.py
@@ -1,10 +1,10 @@
-RED = (0, 255, 0)
-GREEN = (255, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-PURPLE = (0, 75, 130)
-ORANGE = (106, 253, 2)
+PURPLE = (75, 0, 130)
+ORANGE = (253, 106, 2)
 WHITE = (255, 255, 255)
-YELLOW = (150, 255, 0)
+YELLOW = (255, 150, 0)
 OFF = (0, 0, 0)
 
 COLORS = {"red": RED, "green": GREEN, "blue": BLUE, "purple": PURPLE, "orange": ORANGE, "white": WHITE,
