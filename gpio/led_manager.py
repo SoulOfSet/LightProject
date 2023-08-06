5aebab07393bb8dcec4a48e5f28b00aca45d4440
Jacob Sabella <jacobsabella@outlook.com>
2023-08-06 08:18:59 -0500

    Mode and type

--- gpio/led_manager.py
+++ gpio/led_manager.py
@@ -75,7 +75,7 @@ class LedManager:
 
     def start_procedure(self, procedure):
         self.mode = "PROCEDURE"
-        self.type = type(procedure).getName()
+        self.type = procedure
         if procedure in self.available_procedures:
             self.stop_thread_if_running()
             self.curr_thread = self.available_procedures[procedure]()
